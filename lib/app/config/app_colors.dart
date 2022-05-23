6e8221544ca460c228e82016f50a463f1cd69126
deepakr <deepak.r@Turno.club>
2022-05-23 16:04:21 +0530

    testing

--- lib/app/config/app_colors.dart
+++ lib/app/config/app_colors.dart
@@ -5,4 +5,5 @@ import 'package:turno_customer_application/app/extensions/color.dart';
 class AppColors {
   static Color primary = HexColor("05101A");
   static Color lightGray = HexColor("D3D3D3");
+  static Color darkGray = HexColor("D3D3D3");
 }
