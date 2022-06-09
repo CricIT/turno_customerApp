ad9ae28f46d83da71836570178c6a8b8d825c014
Gopal Gaur <gopal.gaur@turno.club>
2022-06-10 02:50:49 +0530

    Login screen code cleaned

--- lib/presentation/pages/login.dart
+++ lib/presentation/pages/login.dart
@@ -4,17 +4,17 @@ import 'package:turno_customer_application/app/config/app_colors.dart';
 import 'package:get/get.dart';
 import 'package:turno_customer_application/app/config/constant.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:turno_customer_application/app/routes/app_route.dart';
 import 'package:turno_customer_application/presentation/controllers/auth/login_controller.dart';
+import 'package:turno_customer_application/presentation/widgets/custom_button.dart';
+import 'package:turno_customer_application/presentation/widgets/custom_text_field.dart';
 
 class LoginPage extends GetView<LoginController> {
-  final _phoneController = TextEditingController();
-  LoginPage();
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: AppColors.whiteColor,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
@@ -23,126 +23,14 @@ class LoginPage extends GetView<LoginController> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                const SizedBox(
-                  height: 16,
-                ),
-                const Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: AppColors.blackColor,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                CarouselSlider(
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    aspectRatio: 1.5,
-                    enlargeCenterPage: true,
-                  ),
-                  items: imageSliders,
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                Container(
-                    constraints: const BoxConstraints(maxWidth: 500),
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: const TextSpan(children: <TextSpan>[
-                        TextSpan(
-                            text: 'We will send you an ',
-                            style: TextStyle(
-                                color: AppColors.blackColor, fontSize: 17)),
-                        TextSpan(
-                            text: 'One Time Password ',
-                            style: TextStyle(
-                                color: AppColors.blackColor,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17)),
-                        TextSpan(
-                            text: 'on this mobile number',
-                            style: TextStyle(
-                                color: AppColors.blackColor, fontSize: 17)),
-                      ]),
-                    )),
-                const SizedBox(
-                  height: 16,
-                ),
-                Container(
-                  height: 50,
-                  margin: const EdgeInsets.all(10),
-                  child: TextField(
-                    controller: _phoneController,
-                    decoration: const InputDecoration(
-                      prefixText: '+91',
-                      labelText: "Enter your number",
-                      labelStyle: TextStyle(color: AppColors.placeholderColor),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(
-                          color: AppColors.subTextColor,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                _buildLoginText(),
+                _buildCarouselSlider(),
+                _otpText(),
+                _buildPhoneInputField(),
                 const Spacer(),
-                Container(
-                  constraints: const BoxConstraints(maxWidth: 500),
-                  child: RaisedButton(
-                    onPressed: () {
-                      controller.signUpWith(_phoneController.text);
-                      Get.toNamed(AppRoutes.OTP);
-                    },
-                    color: AppColors.primaryColor,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(14),
-                      ),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8, horizontal: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const Text(
-                            'Next',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: const BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20)),
-                              color: AppColors.primaryColorLight,
-                            ),
-                            child: const Icon(
-                              Icons.arrow_forward_ios,
-                              color: Colors.white,
-                              size: 16,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                _nextButton(),
                 const SizedBox(
-                  height: 30,
+                  height: 48,
                 ),
               ],
             ),
@@ -152,18 +40,132 @@ class LoginPage extends GetView<LoginController> {
     );
   }
 
+  Widget _buildLoginText() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      child: Center(
+        child: Text(
+          'login'.tr,
+          style: TextStyle(
+            color: AppColors.blackColor,
+            fontSize: 32,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCarouselSlider() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 16),
+      child: CarouselSlider(
+        options: CarouselOptions(
+          viewportFraction: 1,
+          autoPlay: true,
+          aspectRatio: 1.5,
+          padEnds: true,
+          enlargeCenterPage: true,
+          enlargeStrategy: CenterPageEnlargeStrategy.scale,
+        ),
+        items: imageSliders,
+      ),
+    );
+  }
+
+  Widget _otpText() {
+    return Container(
+      constraints: const BoxConstraints(maxWidth: 500),
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 16,
+      ),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          children: <TextSpan>[
+            TextSpan(
+              text: 'login_1'.tr,
+              style: TextStyle(
+                color: AppColors.blackColor,
+                fontSize: 17,
+              ),
+            ),
+            TextSpan(
+              text: 'login_2'.tr,
+              style: TextStyle(
+                color: AppColors.blackColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
+            ),
+            TextSpan(
+              text: 'login_3'.tr,
+              style: TextStyle(
+                color: AppColors.blackColor,
+                fontSize: 17,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPhoneInputField() {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.all(10),
+      child: CustomTextField(
+        textController: controller.phoneController,
+        keyboardInputType: TextInputType.phone,
+        prefixText: '+91',
+        hintText: "enter_number".tr,
+      ),
+    );
+  }
+
+  Widget _nextButton() {
+    return CustomButton(
+      buttonAction: () {
+        if (controller.phoneController.text.length == 10) {
+          controller.signUpWith(controller.phoneController.text);
+        } else {
+          Get.defaultDialog(
+            title: 'alert'.tr,
+            middleText: 'ten_digit'.tr,
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text('ok'.tr),
+              ),
+            ],
+          );
+        }
+      },
+      child: Center(
+        child: Text(
+          "next".tr.toUpperCase(),
+          style: TextStyle(
+            color: AppColors.whiteColor,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
   final List<Widget> imageSliders = Constants.loginPageImages
       .map(
-        (item) => Card(
-          elevation: 4,
-          shadowColor: Colors.grey.shade50,
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-            child: Stack(
-              children: [
-                Image.asset(item, fit: BoxFit.cover, width: 1000.0),
-              ],
-            ),
+        (item) => ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+          child: Stack(
+            children: [
+              Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+            ],
           ),
         ),
       )
