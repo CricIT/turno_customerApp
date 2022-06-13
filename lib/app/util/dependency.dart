9dbec5565d7ed00ee6fc39e5dcf657bc860def3f
Gopal Gaur <gopal.gaur@turno.club>
2022-06-13 23:53:10 +0530

    Dependency injection and landing page controllers

--- lib/app/util/dependency.dart
+++ lib/app/util/dependency.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:turno_customer_application/data/repositories/login_repository.dart';
 import 'package:turno_customer_application/data/repositories/otp_repository.dart';
+import 'package:turno_customer_application/presentation/controllers/landing_page/more_controller.dart';
 
 class DependencyCreator {
   static init() {
     Get.lazyPut(() => LoginRepositoryIml());
     Get.lazyPut(() => OtpRepositoryIml());
+    Get.lazyPut(() => MoreController());
   }
 }
--- lib/presentation/controllers/landing_page/landing_page_binding.dart
+++ lib/presentation/controllers/landing_page/landing_page_binding.dart
@@ -1,18 +1,17 @@
-
-
-
-
-
 import 'package:get/get.dart';
-
+import 'package:turno_customer_application/presentation/controllers/landing_page/loan_controller.dart';
+import 'package:turno_customer_application/presentation/controllers/landing_page/more_controller.dart';
+import 'package:turno_customer_application/presentation/controllers/landing_page/profile_controller.dart';
+import 'package:turno_customer_application/presentation/controllers/landing_page/support_controller.dart';
 import 'landing_page_controller.dart';
 
-class LandingPageBinding extends Bindings{
-
+class LandingPageBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(LandingPageController());
+    Get.lazyPut(() => ProfileController(), fenix: true);
+    Get.lazyPut(() => LoanController(), fenix: true);
+    Get.lazyPut(() => SupportController(), fenix: true);
+    Get.lazyPut(() => MoreController(), fenix: true);
   }
-
-
 }
